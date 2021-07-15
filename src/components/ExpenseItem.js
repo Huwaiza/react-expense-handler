32f17d67786aee88b93b02ce7fee88470a6bf0bd
Huwaiza <muhammadhuwaizatahir@gmail.com>
2021-07-15 13:38:09 +0500

    making tasks more component based and focused like expense date has now separate component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,11 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
 
-    var month = props.date.toLocaleString('en-US', { month: 'long' });
-    var day = props.date.toLocaleString('en-US', { day: '2-digit' });
-    var year = props.date.getFullYear();
     return (
         <div className="expense-item">
-            <div>
-                <div>
-                    {month}
-                </div>
-                <div>
-                    {day}
-                </div>
-                <div>
-                    {year}
-                </div>
-            </div>
+            <ExpenseDate date={props.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>
                     {props.title}
