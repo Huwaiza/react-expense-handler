d14956085ebf5105f1f6fbb5084ce3004f95ef09
Huwaiza <muhammadhuwaizatahir@gmail.com>
2021-07-15 17:51:30 +0500

    states are imbedded as well in expense items so states and props are imbedded for now

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,28 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import React, {useState} from 'react';
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+    const changeTitleHandler = () => {
+        setTitle("Updated Title!")
+    }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>
-                    {props.title}
+                    {title}
                 </h2>
                 <div className="expense-item__price">
                     {props.amount} pkr
                 </div>
+                <button onClick={changeTitleHandler}>
+                    Change Title 
+                </button>
             </div>
         </Card>
     );
