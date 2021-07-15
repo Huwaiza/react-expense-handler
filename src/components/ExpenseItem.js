ed0beb86d2bb2fbd7bba93483c8e226659829de3
Huwaiza <muhammadhuwaizatahir@gmail.com>
2021-07-15 14:33:41 +0500

    acheived composition

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,11 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 function ExpenseItem(props) {
 
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>
@@ -14,7 +15,7 @@ function ExpenseItem(props) {
                     {props.amount} pkr
                 </div>
             </div>
-        </div>
+        </Card>
     );
 }
 
