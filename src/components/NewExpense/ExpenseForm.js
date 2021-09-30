75f5250a0ad83003826ad90d02279ff9aabb81be
Huwaiza <muhammadhuwaizatahir@gmail.com>
2021-10-01 00:29:53 +0500

    classname fixed and onchange on input

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,18 +1,23 @@
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+
+    const titleChangeHandler = () => {
+        console.log("title changed")
+    };
+
     return (
         <form>
             <div className="new-expense__controls">
-                <div classNam="new-expense__control">
+                <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" />
+                    <input type="text" onChange={titleChangeHandler}/>
                 </div>
-                <div classNam="new-expense__control">
+                <div className="new-expense__control">
                     <label>Amount</label>
                     <input type="number" min="0.01" step="0.01" />
                 </div>
-                <div classNam="new-expense__control">
+                <div className="new-expense__control">
                     <label>Date</label>
                     <input type="date" min="2019-01-01" max="2022-12-31 " />
                 </div>
